e45181a3b89e84b894fb7d698a6d5087a3f06c14
Jinjerana <125341676+Jinjerana@users.noreply.github.com>
2023-09-04 07:18:23 +0200

    HW2_1_6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
+import React, {Component} from 'react';
+
+import Forma from './Form/Form'
+import Contacts from './Contacts/Contacts'
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render () {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+    <div>
+  <Forma/>
+
+  <Contacts/>
+  </div> 
+  )
+  }
+}
+
+export default App
---
+++ src/components/Contacts/Contacts.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Contacts = ({ name, id}) => 
+(
+<div className="contacts">
+    {/* <Title className="section-title">{title}</Title>
+    {children} */}
+</div>
+)
+
+export default Contacts
